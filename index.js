ba7823fdb4a08cd5d0dd3b5e53395dc863475cd3
Jonathon <jwbialas@gmail.com>
2022-07-12 11:03:17 -0500

    currated meme list - second set

--- index.js
+++ index.js
@@ -7,10 +7,61 @@ fetch('https://api.imgflip.com/get_memes')
     .then(res => res.json())
     .then(resObj => {
  //       resObj.data.memes.forEach((memeObj) => {
-              const memeArray = [resObj.data.memes[7],
+              const memeArray = [
+              resObj.data.memes[7],
               resObj.data.memes[8],
               resObj.data.memes[10],
               resObj.data.memes[11],
+              resObj.data.memes[15],
+              resObj.data.memes[20],
+              resObj.data.memes[21],
+              resObj.data.memes[25],
+              resObj.data.memes[32],
+              resObj.data.memes[33],
+              resObj.data.memes[38],
+              resObj.data.memes[39],
+              resObj.data.memes[42],
+              resObj.data.memes[45],
+              resObj.data.memes[46],
+              resObj.data.memes[47],
+              resObj.data.memes[48],
+              resObj.data.memes[49],
+              resObj.data.memes[52],
+              resObj.data.memes[53],
+              resObj.data.memes[54],
+              resObj.data.memes[55],
+              resObj.data.memes[56],
+              resObj.data.memes[58],
+              resObj.data.memes[59],
+              resObj.data.memes[61],
+              resObj.data.memes[62],
+              resObj.data.memes[64],
+              resObj.data.memes[65],
+              resObj.data.memes[66],
+              resObj.data.memes[67],
+              resObj.data.memes[68],
+              resObj.data.memes[70],
+              resObj.data.memes[71],
+              resObj.data.memes[74],
+              resObj.data.memes[75],
+              resObj.data.memes[76],
+              resObj.data.memes[77],
+              resObj.data.memes[78],
+              resObj.data.memes[79],
+              resObj.data.memes[81],
+              resObj.data.memes[82],
+              resObj.data.memes[84],
+              resObj.data.memes[85],
+              resObj.data.memes[86],
+              resObj.data.memes[87],
+              resObj.data.memes[88],
+              resObj.data.memes[89],
+              resObj.data.memes[91],
+              resObj.data.memes[94],
+              resObj.data.memes[96],
+              resObj.data.memes[97],
+              resObj.data.memes[99],
+
               
               ]
  //             displayMemes(memeObj)
@@ -22,15 +73,18 @@ fetch('https://api.imgflip.com/get_memes')
  //   } )   
         
 function displayMemes(memeArray) {
+    memeArray.forEach((memeObj)=>{
+        
+    
     const memeContainer = document.querySelector(".meme-container")
     const nameElement = document.createElement('h2')
-    nameElement.textContent = memeArray.name
+    nameElement.textContent = memeObj.name
     const imageElement = document.createElement('img')
-    imageElement.src = memeArray.url
+    imageElement.src = memeObj.url
     memeContainer.append(nameElement, imageElement)
     
     
-}
+    })}
 
     //const memeArray = resObj.data.memes
 
