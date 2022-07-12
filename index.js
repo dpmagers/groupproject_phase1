16b594a29c333e9d24a383408fcf2c6a4e29fe88
Joshua Greer <jtgreer95@gmail.com>
2022-07-12 16:11:03 -0500

    created meme container

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
-// function fetchRequest () {
-// return 
-
-//?_limit=20&_page=3
 fetch('https://api.imgflip.com/get_memes')
     .then(res => res.json())
     .then(resObj => {
@@ -64,14 +60,15 @@ fetch('https://api.imgflip.com/get_memes')
               ]
             
         
- //             displayMemes(memeObj)
- //             console.log(resObj.data.memes)
             displayMemes(memeArray)
             console.log(memeArray)
+            // console.log(resObj.data.memes)
         }) 
         
  //   } )   
-        
+ const memeTitle = document.querySelector("div#meme-tank h3")      
+ const memeImg =  document.querySelector("div#meme-tank img")
+
 function displayMemes(memeArray) {
     memeArray.forEach((memeObj)=>{
     const memeContainer = document.querySelector(".meme-container")
@@ -83,9 +80,8 @@ function displayMemes(memeArray) {
       
     // click listener for meme display
         imageElement.addEventListener('click',()=>{
-            const memeTitle = document.querySelector("div#meme-tank h3") 
+            
             memeTitle.innerHTML = memeObj.name 
-            const memeImg =  document.querySelector("div#meme-tank img")
             memeImg.src = memeObj.url
         })
     })}
@@ -108,88 +104,39 @@ form.addEventListener('submit',(e)=>{
 const btn = document.getElementById('btn') 
 btn.addEventListener("click",(e)=>{
     console.log('submitted')
+    const topOutput = document.getElementById('top-text-output')
+    const bottomOutput = document.getElementById('bottom-text-output')
+    
+    
+    const newMemeArray = []
+    newMemeArray[0] = memeTitle
+    newMemeArray[1] = memeImg
+    newMemeArray[2] = topOutput
+    newMemeArray[3] = bottomOutput
+
+    const newMemeContainer = document.getElementById('created-and-favorited-memes')
+    const newMemeDiv = document.createElement('div')
+    
+    
+    const newMemeTitle = document.createElement('h2')
+    newMemeTitle.innerHTML = memeTitle
+
+    // const newMemeImg = document.createElement('img')
+    // newMemeImg.src = memeImg
+
+    // const newMemeTopOutput = document.createElement('p').setAttribute("id", "new-meme-top-output")
+    // newMemeTopOutput.textContent = topOutput
+
+    // const newMemeBottomOutput = document.createElement('p').setAttribute("id", "new-meme-bottom-output")
+    // newMemeBottomOutput.textContent = bottomOutput
+
+    newMemeContainer.append(newMemeDiv)
+    newMemeDiv.append(newMemeTitle)
+
+
+    
+    
+    
 })   
 
-    //const memeArray = resObj.data.memes
-
-
-
-//resObj is passed into an arrow function 
-//argument is res.Obj.data.meme
-//do a forEach method on the resObj.data.meme array 
-//pass this memeArray into the meme container (either in a seperate function that is invoked in the fetch
-//or with the "display memes" functionality/function living in the fetch)
-
-//JOSH'S SOLUTION FOR FETCH 
-// fetch("https://www.dnd5eapi.co/api/monsters")
-// .then(response => response.json())
-// .then(monsters => displayMonsters(monsters))
-
-// function displayMonsters(monsters){
-//   monsters.results.forEach(array => newMonsters(array))
-
-// }
-// function newMonsters(array){
-//     console.log("hello")
-
-
-
-// const memeContainer = document.getElementsByClassName("meme-container")
-// const nameElement = document.createElement('h2')
-// const imageElement = document.createElement('img')
-// memeContainer.append(nameElement, imageElement)    
-        
-//         = memeArray)
-        
-//      //   (resObj.data.memes.forEach(displayMemes)))
-
-
-// //console.log()
-//     }
-//     console.log(memeArray)
-
-//drop down menu to display names to select 
-//click to submit form 
-
-    //h2 = name 
-    //url = image 
-
-    // box_count: 2
-    // height: 1200
-    // id: "181913649"
-    // name: "Drake Hotline Bling"
-    // url: "https://i.imgflip.com/30b1gx.jpg"
-    // width: 1200
-
-
-       // console.log(resObj.data.memes))
-    
-    
-    // const fruits = ["apple", "orange", "cherry"];
-    // fruits.forEach(myFunction);
-    
-    //array = resObj.data.memes
-
-    //     {
-    //     displayMemes(resObj) 
-
-    //     console.log(resObj.data.memes)
-
-    // })
-    
-// displayMemes() {
-
-// }
-
-
-// const header = document.querySelector('h1')
-// header.addEventListener('click', e => {
-//     fetchRequest()
-// })
-
-
-
-
-
-
-
+   
